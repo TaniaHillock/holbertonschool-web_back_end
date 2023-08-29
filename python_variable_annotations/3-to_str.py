d45e79b92cc40951bbde3d8f710537dd58dcd080
TaniaHillock <tania.hillock@gmail.com>
2023-08-29 04:57:39 -0600

    Basic annotationas - to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Type-annotaded function - to_str"""
+
+n: float = 98.854
+
+def to_str(n: float) -> str:
+    """Returns the string representation of the float"""
+    string = str(n)
+    return str(string)
