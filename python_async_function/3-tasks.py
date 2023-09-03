1d6e35e97449315b8be48e04ee4c3744ff9102a4
TaniaHillock <tania.hillock@gmail.com>
2023-09-03 01:19:45 -0600

    Pycodestyle fixes

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Import wait_random from 0-basic_async_syntax.
-Not an async function, but a regular function task_wait_random 
+Not an async function, but a regular function task_wait_random
 that takes an integer max_delay and returns a asyncio.Task"""
 import asyncio
 import random
@@ -13,6 +13,3 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """Returns a asyncio.Task"""
     return asyncio.create_task(wait_random(max_delay))
-
-
-
