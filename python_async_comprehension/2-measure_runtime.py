11bfb1fa598814df3a4acb49a6084663c955ff0e
TaniaHillock <tania.hillock@gmail.com>
2023-09-03 02:07:39 -0600

    Runtime

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -3,6 +3,7 @@
 and write a measure_runtime coroutine that will
 execute async_comprehension four times in parallel using asyncio.gather.
 measure_runtime measures the total runtime and return it"""
+
 import asyncio
 import random
 from typing import Generator
@@ -11,8 +12,10 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """The coroutine will collect 10 random numbers using an async comprehensing over async_generator,
+    """The coroutine will collect 10 random numbers
+    using an async comprehensing over async_generator,
     then return the 10 random numbers"""
+
     start = asyncio.get_event_loop().time()
     await asyncio.gather(*(async_comprehension() for i in range(4)))
     end = asyncio.get_event_loop().time()
