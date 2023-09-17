2141bf52371e590583fc298942066b6bb5ab5900
TaniaHillock <tania.hillock@gmail.com>
2023-09-17 10:16:29 +0000

    Vroom fix

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -4,13 +4,13 @@ const _color = Symbol('color');
 
 class Car {
   constructor(brand, motor, color) {
-    this[_brand] = brand;
-    this[_motor] = motor;
-    this[_color] = color;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   cloneCar() {
-    return new this.constructor(this[_brand], this[_motor], this[_color]);
+    return new this.constructor(this._brand, this._motor, this._color);
   }
 }
 
